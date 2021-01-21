0f4ea6b36d52cc8aa1ae9efec787e60175d1b197
Tate <tatekennington@gmail.com>
2021-01-21 21:27:22 +1300

    Started working on gui

---
+++ TensionBar.gd
@@ -0,0 +1,14 @@
+tool
+
+extends Control
+
+export(float) var value setget set_value;
+
+func set_value(new_value: float):
+	value = new_value;
+	update();
+
+func _draw():
+	draw_rect(Rect2(rect_pivot_offset, rect_size), Color("#CCCCCC"))
+	draw_rect(Rect2(rect_pivot_offset + Vector2(2, 2), rect_size - Vector2(4, 4)), Color("#777"))
+	draw_rect(Rect2(Vector2(rect_pivot_offset.x + 0.5*rect_size.x, rect_pivot_offset.y+2), Vector2(value/100*(rect_size.x/2-2), rect_size.y-4)), Color.red)
