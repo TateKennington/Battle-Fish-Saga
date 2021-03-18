3bb5057b5fe3a7c98196de6c797ef03f5f33be35
Tate <tatekennington@gmail.com>
2021-03-18 21:28:39 +1300

    Scrolling menu

--- GUI/BattleGUI.gd
+++ GUI/BattleGUI.gd
@@ -7,16 +7,31 @@ signal choose_move;
 onready var tension_bar: TensionBar = $StatsPanel/Tension/TensionBar
 onready var distance_bar: TensionBar = $StatsPanel/Distance/DistanceBar
 
+const window_size = 3;
+
+var top = 0;
 var selected = 0;
+var moves_list;
 	
-func set_moves_list(moves: Array):
-	for move in moves:
+func set_moves_list(moves: Dictionary):
+	moves_list = moves;
+	moves_list["basics"].append("special")
+	for move in moves["basics"]:
 		$MovesPanel.add_move(move)
 	update_indicators();
 
 func update_indicators():
 	for i in range($MovesPanel/Indicators.get_child_count()):
+		$MovesPanel/Indicators.get_child(i).visible = false;
 		$MovesPanel/Indicators.get_child(i).deactivate();
+	for i in range($MovesPanel/MoveLabels.get_child_count()):
+		$MovesPanel/MoveLabels.get_child(i).visible = false;
+		
+	for i in range(top, top+window_size):
+		$MovesPanel/Indicators.get_child(i).visible = true;
+	for i in range(top, top+window_size):
+		$MovesPanel/MoveLabels.get_child(i).visible = true;
+		
 	$MovesPanel/Indicators.get_child(selected).activate();
 
 func set_tension(tension: float):
@@ -33,9 +48,16 @@ func _input(event):
 		if key.scancode == KEY_DOWN:
 			selected = selected + 1;
 		if key.scancode == KEY_SPACE:
+			if moves_list["basics"][selected] == "special":
+				print("TODO: Special moves menu")
+				return
 			emit_signal("choose_move", selected);
 			deactivate();
 		selected = clamp(selected, 0, $MovesPanel/Indicators.get_child_count()-1)
+		if selected < top:
+			top = selected
+		if selected >= top + window_size:
+			top = selected - window_size + 1
 		update_indicators()
 
 func activate():
--- Player/Player.gd
+++ Player/Player.gd
@@ -3,12 +3,18 @@ extends Node2D
 class_name Player
 
 onready var abilities = $Abilities.get_children();
+onready var specials = $SpecialAbilities.get_children();
 
 func perform_move(index: int):
 	abilities[index].ability();
 
-func get_moves_list() -> Array:
-	var result = [];
+func get_moves_list() -> Dictionary:
+	var result = {
+		"basics": [],
+		"specials": []
+	};
 	for ability in abilities:
-		result.push_back(ability.get_name().to_lower())
+		result["basics"].push_back(ability.get_name().to_lower())
+	for special in specials:
+		result["specials"].push_back(special.get_name().to_lower())
 	return result;
