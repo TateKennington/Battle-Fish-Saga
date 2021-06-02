8ead1c797db40778694b4555f228e36b4f431a61
Tate <tatekennington@gmail.com>
2021-06-02 23:02:11 +1200

    Implement spending special

--- GUI/Components/SpecialBar/SpecialBar.gd
+++ GUI/Components/SpecialBar/SpecialBar.gd
@@ -5,7 +5,27 @@ var current_index = 0;
 func push_special(special: int):
 	get_child(current_index).current_state = special;
 	current_index += 1;
-	
+
+func spend(num_tension: int, num_slack: int):
+	var new_index: int = current_index - num_tension - num_slack;
+	for index in range(0, current_index):
+		var child: Special_Counter = get_child(index);
+		var state = child.current_state
+		if num_tension > 0 && state == Special_Counter.State.Tense:
+			num_tension -= 1;
+			child.current_state = Special_Counter.State.Inactive
+		if num_slack > 0 && state == Special_Counter.State.Slack:
+			num_slack -= 1;
+			child.current_state = Special_Counter.State.Inactive
+	var empty = 0;
+	for index in range(0, current_index):
+		while get_child(empty).current_state != Special_Counter.State.Inactive && empty < current_index:
+			empty += 1
+		if empty < index:
+			get_child(empty).current_state = get_child(index).current_state
+			get_child(index).current_state = Special_Counter.State.Inactive
+	current_index = new_index
+				
 func clear():
 	for child in get_children():
 		child.current_state = Special_Counter.State.Inactive
